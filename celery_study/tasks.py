7fe63c61fc9da412270f9eb7e0f37a3c4430376f
uamrws <uamr567@sina.com>
2022-07-21 11:16:04 +0800

    feat: celery的request钩子函数测试

--- celery_study/tasks.py
+++ celery_study/tasks.py
@@ -1,14 +1,29 @@
 import json
 from collections import UserDict
 
-from celery import Celery
+from celery import Celery, Task
+from celery.worker.request import Request
 
 uri = 'redis://localhost:6379/7'
 app = Celery('tasks', backend='redis://localhost:6379/2', broker=uri)
 app.conf.enable_utc = True
+import time
 
-from time import sleep
 
-@app.task(bind=True)
-def demo2(self, a):
-    print(a)
+class MyRequset(Request):
+    def on_timeout(self, soft, timeout):
+        print(self.args)
+
+    def on_failure(self, exc_info, send_failed_event=True, return_ok=False):
+        return None
+
+
+class MyTask(Task):
+    Request = MyRequset
+
+    def run(self, a, b):
+        time.sleep(3)
+        print(a + b)
+
+
+add = app.register_task(MyTask())
